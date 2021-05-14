73fbbf9fad8b9abf6393405d9f979606e6b4420a
BapiGso <59081856+BapiGso@users.noreply.github.com>
2021-05-14 19:38:18 +0800

    Update page.php

--- page.php
+++ page.php
@@ -8,26 +8,7 @@
  * @FilePath: /Diaspora/page.php
  */
 ?>
-<!DOCTYPE html>
-<html lang="zh-CN">
-<head>
-	<meta charset="<?php $this->options->charset(); ?>">
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="google" content="notranslate" />
-    <title><?php $this->archiveTitle(array(
-            'category'  =>  _t('分类 %s 下的文章'),
-            'search'    =>  _t('包含关键字 %s 的文章'),
-            'tag'       =>  _t('标签 %s 下的文章'),
-            'author'    =>  _t('%s 发布的文章')
-        ), ''); ?></title>
-	<link rel="stylesheet" href="<?php $this->options->themeUrl('assets/css/Diaspora.css'); ?>">
-	<?php $this->header(); ?>
-<head>
+<?php $this->need('component/header.php'); ?>
     
 <div id="single" class="page">
     <div id="top">
