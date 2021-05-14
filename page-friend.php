50b43f247de80a73b5ab95474af8a8684442e838
BapiGso <59081856+BapiGso@users.noreply.github.com>
2021-05-14 19:38:58 +0800

    Update page-friend.php

--- page-friend.php
+++ page-friend.php
@@ -1,47 +1,48 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php
 /**
- * 友情链接独立页面模板
+ * 时间线独立页面模板
  * @Date: 2020-04-29 20:06:36
  * @Author: Bapi
  * @package custom
- * @FilePath: /diaspora/page-friend.php
+ * @FilePath: /diaspora/page-timeline.php
  */
 ?>
-<!DOCTYPE html>
-<html lang="zh-CN">
-<head>
-	<meta charset="<?php $this->options->charset(); ?>">
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="google" content="notranslate" />
-    <title><?php $this->archiveTitle(array(
-            'category'  =>  _t('分类 %s 下的文章'),
-            'search'    =>  _t('包含关键字 %s 的文章'),
-            'tag'       =>  _t('标签 %s 下的文章'),
-            'author'    =>  _t('%s 发布的文章')
-        ), ''); ?></title>
-	<link rel="stylesheet" href="<?php $this->options->themeUrl('assets/css/Diaspora.css'); ?>">
-	<?php $this->header(); ?>
-<head>
-    
+<?php $this->need('component/header.php'); ?>
+
 <div id="single" class="page">
     <div id="top">
-        <a class="image-icon" href="javascript:history.back()"></a>
+        <a class="image-icon" href="javascript:history.back()"></a><a href="<?php $this->options->siteUrl(); ?>">
     </div>
-
+    
     <div class="section" style="left: 0;">
 	    <div class="article">
-                <div>
-			    <ul class="friend">
+            <div>
+            <?php $this->widget('Widget_Contents_Post_Recent', 'pageSize=10000')->to($archives);
+    $year=0; $mon=0; $i=0; $j=0;
+    $output = '<div class="content">';
+    while($archives->next()):
+        $year_tmp = date('Y',$archives->created);
+        $mon_tmp = date('m',$archives->created);
+        $y=$year; $m=$mon;
+        if ($mon != $mon_tmp && $mon > 0) $output .= '</ul></li>';
+        if ($year != $year_tmp && $year > 0) $output .= '</ul>';
+        if ($year != $year_tmp) {
+            $year = $year_tmp;
+            $output .= '<h2><strong>'. $year .' 年 <strong></h2><ul>'; //输出年份
+        }
+        if ($mon != $mon_tmp) {
+            $mon = $mon_tmp;
+            $output .= '<li><span>'. $mon .' 月</span><ul>'; //输出月份
+        }
+        $output .= '<li>'.date('d日: ',$archives->created).'<a href="'.$archives->permalink .'">'. $archives->title .'</a> </li>'; //输出文章日期和标题
+    endwhile;
+    $output .= '</ul></li></ul></div>';
+    echo $output;
+?>
         		    <?php $this->content(); ?>
-				</ul>
                 </div>
-
+                
                 <div class="comment-wrap">
                     <?php $this->need('component/comments.php'); ?>                    
                 </div>
